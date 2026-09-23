d6041bdcc834615d5ba2c66f39ffa4506d9fbba0
greysheep <Grey_sheep>
2026-09-23 16:26:41 +0800

    载入法线数据

--- src/model.h
+++ src/model.h
@@ -7,11 +7,15 @@ class Model{
 
 private:
     std::vector<Vector3f>  vertices;
+    std::vector<Vector3f>  norm_coords;
     std::vector<std::vector<int>> faces;
+    std::vector<std::vector<int>> normals;
 
 public:
     Model(const char* filename);
     ~Model();
-    std::vector<Vector3f> getVertices(){return vertices;}
-    std::vector<std::vector<int>> getFaces(){return faces;}
+    std::vector<Vector3f> getVertices() const& {return vertices;}
+    std::vector<Vector3f> getNormCoords() const& {return norm_coords;}
+    std::vector<std::vector<int>> getFaces() const& {return faces;}
+    std::vector<std::vector<int>> getNormals() const& {return normals;}
 };
